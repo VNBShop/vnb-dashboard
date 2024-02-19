18acbbaa208abd24d8c906f6f9aaac5de1078170
jungjung <dungmv.now@gmail.com>
2024-02-19 11:33:20 +0700

    update color site

--- src/contents/stores/header.tsx
+++ src/contents/stores/header.tsx
@@ -44,16 +44,16 @@ export default function StoresHeader({
         <HeaderSection title="Stores manage">
           <section className="flex items-center gap-4">
             <div
-              className="flex h-[36px] w-[36px] items-center justify-center rounded border hover:cursor-pointer hover:bg-black hover:text-white"
+              className="p-2 px-1 hover:cursor-pointer hover:text-secondary"
               onClick={() => refetch()}
             >
               <Icon name="Reload" size={16} />
             </div>
             <div
-              className="flex h-[36px] w-[36px] items-center justify-center rounded border  hover:cursor-pointer hover:bg-black hover:text-white"
+              className=" p-2 px-1 hover:cursor-pointer hover:text-secondary"
               onClick={() => setFilter((prev) => !prev)}
             >
-              <Icon name="Filter" size={16} />
+              <Icon name="Filter" size={16} color={filter ? '#ff2461' : ''} />
             </div>
             <div
               onClick={() => {
