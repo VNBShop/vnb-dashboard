074df17d8599bc076c832870fe8e7b6a43c8652d
jungjung <dungmv.now@gmail.com>
2024-02-21 21:43:11 +0700

    update refresh token

--- src/api/private/useAxios.ts
+++ src/api/private/useAxios.ts
@@ -2,34 +2,60 @@
 
 import { useEffect } from 'react'
 
-import { useSession } from 'next-auth/react'
+import axios from 'axios'
+import { Session } from 'next-auth'
+import { getSession, signOut, useSession } from 'next-auth/react'
 
 import axiosPrivate from './axios'
-import { useRefreshToken } from './useRefreshToken'
 
 export default function useAxiosPrivate() {
-  const { data: session } = useSession()
-  const refreshToken = useRefreshToken()
+  const { data: session, update } = useSession()
+  // const refreshToken = useRefreshToken()
 
   useEffect(() => {
-    if (!session) return
+    // let isRefreshing = false
 
-    let isRefreshing = false
+    if (!session) return
 
     const responseIntercept = axiosPrivate.interceptors.response.use(
       (res) => res,
       async (err) => {
         const prevReq = err.config
         if (err?.response?.status === 401 && !prevReq?.sent) {
-          if (!isRefreshing) {
-            isRefreshing = true
-            await refreshToken.mutate()
-            isRefreshing = false
+          try {
+            const response = await axios.post(
+              `${process.env.NEXT_SERVER_API_SERVICE}/user-service/api/v1/account/refresh-token`,
+              {
+                refreshToken: session?.user?.refreshToken,
+              }
+            )
+
+            if (response?.data?.success && !!update && !!session) {
+              await update({
+                ...session,
+                user: {
+                  ...session.user,
+                  accessToken: response?.data?.metadata?.accessToken,
+                  refreshToken: response?.data?.metadata?.refreshToken,
+                },
+              })
+            }
+          } catch (error: any) {
+            if (error?.response?.status) await signOut()
           }
+
+          // if (!isRefreshing) {
+          //   isRefreshing = true
+          // await refreshToken.mutate()
+          //   isRefreshing = false
+          // }
           prevReq.sent = true
 
-          prevReq.headers['Authorization'] =
-            `Bearer ${session?.user.accessToken}`
+          const response = await getSession()
+          const user = response?.user as Session['user']
+
+          prevReq.headers['Authorization'] = `Bearer ${user.accessToken}`
+
           return axiosPrivate(prevReq)
         }
         return Promise.reject(err)
@@ -40,6 +66,7 @@ export default function useAxiosPrivate() {
       // axiosPrivate.interceptors.request.eject(requestIntercept)
       axiosPrivate.interceptors.response.eject(responseIntercept)
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session])
 
--- src/api/private/useSession.ts
+++
@@ -1,5 +0,0 @@
-import { getSession } from 'next-auth/react'
-
-export async function getAppSession() {
-  return await getSession()
-}
